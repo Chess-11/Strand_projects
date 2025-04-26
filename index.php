73d6ac782180ae456b31409b99e9ff6a8290b615
CHESS <chesterilao001@gmail.com>
2025-04-26 18:44:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- saved from url=(0043)http://localhost/Strand-Recommender-System/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strand Recommender System</title>
+    <link rel="stylesheet" href="./index_files/Student-Styles.css">
+</head>
+
+<body>
+
+    <header class="header">
+        <img src="./index_files/CJPS.png" alt="CJPS Logo" class="logo-header">
+        <h3>Strand Recommender System</h3>
+    </header>
+
+    <main class="content">
+        <div class="box">
+            <img src="./index_files/CJPS.png" alt="CJPS Icon" class="logo">
+            <h3>Welcome to Child Jesus of Prague Strand Recommender System</h3>
+            <p>
+                This system is designed to help you explore the different Senior High School strand and guide you toward a path that matches your interests and strengths.
+            </p>
+            <a href="http://localhost/Strand-Recommender-System/Login.php" class="btn-start">Start</a>
+        </div>
+    </main>
+
+
+</body></html>
